6e6c30e86d8a80fde32dd7c1aed93aa289bb6620
SeyedAlimousavii <92367027+SeyedAlimousavii@users.noreply.github.com>
2022-01-03 17:05:10 +0330

    aa

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
 
 <?php
-
+ echo $_GET['name1'+'name2'];
 ?>
 <!DOCTYPE html>
 <html>
 <body>
 
-<h2>HTML Forms</h2>
 
-<form action="/action_page.php">
-  <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" ><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" ><br><br>
-  <input type="submit" value="Submit">
+
+    <form action="index.php" method="GET">
+        <label for="fname">First number:</label><br>
+        <input type="text" id="fname" name1="name1" ><br>
+        <label for="lname">Second name:</label><br>
+        <input type="text" id="lname" name2="name2" ><br><br>
+        <input type="submit" value="Submit">
 </form> 
 
 
