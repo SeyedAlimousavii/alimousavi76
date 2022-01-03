d4a96b95d7f554ede8de1c26834ea2143aa7db8a
SeyedAlimousavii <92367027+SeyedAlimousavii@users.noreply.github.com>
2022-01-03 18:01:09 +0330

    aa

--- index.php
+++ index.php
@@ -1,42 +1,28 @@
-
 <?php
- if ($_POST['mode'] == 1) {
-     echo $_POST['num1']+$_POST['num2'];
- }
- elseif ($_POST['mode'] == 2) {
-    echo $_POST['num1']-$_POST['num2'];
- }
- elseif ($_POST['mode'] == 3) {
-    echo $_POST['num1']*$_POST['num2'];
-}
-elseif ($_POST['mode'] == 4) {
-    echo $_POST['num1']/$_POST['num2'];
-}
+
+
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+        <form action="/action_page.php">
+            <label for="fname">First name:</label><br>
+            <input type="text" id="fname" name="fname" value="John"><br>
+            <label for="lname">Last name:</label><br>
+            <input type="text" id="lname" name="lname" value="Doe"><br><br>
+            <input type="submit" value="Submit">
+        </form> 
+
+
+
+        <input type="radio" id="html" name="fav_language" value="HTML">
+        <label for="html">HTML</label><br>
+        <input type="radio" id="css" name="fav_language" value="CSS">
+        <label for="css">CSS</label><br>
+        <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+        <label for="javascript">JavaScript</label><br>
+</head>
 <body>
-
-
-<hr>
-    <form action="index.php" method="POST">
-        <label for="fname">First number:</label><br>
-        <input type="number" id="fname" name="num1" ><br>
-        <label for="lname">Second name:</label><br>
-        <input type="number" id="lname" name="num2" ><br><br>
-        <input type="submit" value="Submit">
-        <hr>
-        <input type="radio" id="sum" name="mode" value="1">
-        <label for="sum">sum</label><br>
-        <input type="radio" id="mines" name="mode" value="2">
-        <label for="mines">mines</label><br>
-        <input type="radio" id="multipul" name="mode" value="3">
-        <label for="multipul">multipul</label>
-        <br>
-        <input type="radio" id="Taghsim" name="mode" value="4">
-        <label for="Taghsim">Taghsim</label>
-</form> 
-
-
+    
 </body>
 </html>
