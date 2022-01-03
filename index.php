8188597c268e58f04392ba9fcfd3d532767473ae
SeyedAlimousavii <92367027+SeyedAlimousavii@users.noreply.github.com>
2022-01-03 18:04:17 +0330

    gg

--- index.php
+++ index.php
@@ -7,20 +7,12 @@
 <head>
         <form action="/action_page.php">
             <label for="fname">Username:</label><br>
-            <input type="text" id="fname" name="fname" ><br>
+            <input type="text" id="fname" name="fname" value=""><br>
             <label for="lname">Password:</label><br>
-            <input type="text" id="lname" name="lname"><br><br>
+            <input type="text" id="lname" name="lname" value=""><br><br>
             <input type="submit" value="Submit">
         </form> 
 
-
-
-        <input type="radio" id="html" name="fav_language" value="HTML">
-        <label for="html">HTML</label><br>
-        <input type="radio" id="css" name="fav_language" value="CSS">
-        <label for="css">CSS</label><br>
-        <input type="radio" id="javascript" name="fav_language" value="JavaScript">
-        <label for="javascript">JavaScript</label><br>
 </head>
 <body>
     
