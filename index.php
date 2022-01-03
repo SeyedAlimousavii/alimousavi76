6e6e098b275a96c187a4dc6c40b59e088346f43f
SeyedAlimousavii <92367027+SeyedAlimousavii@users.noreply.github.com>
2022-01-03 16:54:37 +0330

    aa

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 
 <form action="/action_page.php">
   <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value="John"><br>
+  <input type="text" id="fname" name="fname" ><br>
   <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="Doe"><br><br>
+  <input type="text" id="lname" name="lname" ><br><br>
   <input type="submit" value="Submit">
 </form> 
 
