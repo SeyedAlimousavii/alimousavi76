c9c3fdcdd6c989c8c57e1722b2ec1c0b320d6ffd
SeyedAlimousavii <92367027+SeyedAlimousavii@users.noreply.github.com>
2022-01-03 17:35:26 +0330

    aa

--- index.php
+++ index.php
@@ -14,6 +14,13 @@
         <label for="lname">Second name:</label><br>
         <input type="number" id="lname" name="num2" ><br><br>
         <input type="submit" value="Submit">
+        <hr>
+        <input type="radio" id="html" name="fav_language" value="HTML">
+        <label for="html">HTML</label><br>
+        <input type="radio" id="css" name="fav_language" value="CSS">
+        <label for="css">CSS</label><br>
+        <input type="radio" id="javascript" name="fav_language" value="JavaScript">
+        <label for="javascript">JavaScript</label>
 </form> 
 
 
