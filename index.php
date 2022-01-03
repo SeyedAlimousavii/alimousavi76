2f15038bbf6dc3d550c14a345811d8d7d60130ae
SeyedAlimousavii <92367027+SeyedAlimousavii@users.noreply.github.com>
2022-01-03 17:43:32 +0330

    aa

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 
 <?php
- echo $_GET['num1']+$_GET['num2'];
+ if ($_POST['mode'] == 1) {
+     echo $_POST['num1']+$_POST['num2'];
+ }
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,19 +10,21 @@
 
 
 <hr>
-    <form action="index.php" method="GET">
+    <form action="index.php" method="POST">
         <label for="fname">First number:</label><br>
         <input type="number" id="fname" name="num1" ><br>
         <label for="lname">Second name:</label><br>
         <input type="number" id="lname" name="num2" ><br><br>
         <input type="submit" value="Submit">
         <hr>
-        <input type="radio" id="html" name="fav_language" value="HTML">
-        <label for="html">HTML</label><br>
-        <input type="radio" id="css" name="fav_language" value="CSS">
-        <label for="css">CSS</label><br>
-        <input type="radio" id="javascript" name="fav_language" value="JavaScript">
-        <label for="javascript">JavaScript</label>
+        <input type="radio" id="sum" name="mode" value="1">
+        <label for="sum">sum</label><br>
+        <input type="radio" id="mines" name="mode" value="2">
+        <label for="mines">mines</label><br>
+        <input type="radio" id="multipul" name="mode" value="3">
+        <label for="multipul">multipul</label>
+        <input type="radio" id="Taghsim" name="mode" value="4">
+        <label for="Taghsim">Taghsim</label>
 </form> 
 
 
